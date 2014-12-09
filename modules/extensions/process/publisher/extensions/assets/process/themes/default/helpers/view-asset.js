c5bd98940840cc61c5f0a8fc3493f99d0b43624d
Shiva Balachandran <shiva@wso2.com>
2014-12-09 11:00:22 +0530

    Adding Publisher Contents of Process Center

---
+++ modules/extensions/process/publisher/extensions/assets/process/themes/default/helpers/view-asset.js
@@ -0,0 +1,6 @@
+var resources = function (block, page, area, meta) {
+    return {
+        js: ['jquery.MetaData.js', 'jquery.rating.pack.js', 'async.min.js', 'asset-core.js', 'asset.js', 'moment.min.js', 'porthole.min.js','jquery-1.8.3.min.js','assetReplacer.js'],
+        css: ['assets.css','editablegrid-2.0.1.css','styles.css','font-awesome-ie7.min.css','jslider.css','jslider.round.plastic.css','navigation.css','sort-assets.css'],
+    };
+};
