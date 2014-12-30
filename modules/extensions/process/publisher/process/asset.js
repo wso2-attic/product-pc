022ea2f41b1b48a7f2dc7a13036baf4e18a2f1e9
Shiva Balachandran <shiva@wso2.com>
2014-12-30 09:36:42 +0530

    Fixes for missing license texts, removed unecessary files, spacing for = sign was fixed

--- modules/extensions/process/publisher/process/asset.js
+++ modules/extensions/process/publisher/process/asset.js
@@ -27,7 +27,6 @@ asset.manager = function(ctx) {
     return {
         create: function(options) {
 
-            log.info("Entered");
             var ref = require('utils').time;
             var GovernanceConstants = Packages.org.wso2.carbon.governance.api.util;
             //Check if the options object has a createdtime attribute and populate it 
@@ -37,15 +36,9 @@ asset.manager = function(ctx) {
 
             this._super.create.call(this, options);
 
-            // log.info(options);
             var asset = this.get(options.id);
-            log.info(asset);
-            for (var key in this.registry) {
-                log.info('key: ' + key);
-            }
-            //TODO
-
-            //Adding Associatin for Predecessors
+            
+	    //Adding Associatin for Predecessors
             if (asset.attributes.properties_predecessors != null) {
                 var tempArray1 = asset.attributes.properties_predecessors.split("\,");
 
@@ -53,58 +46,37 @@ asset.manager = function(ctx) {
 
                     var preAsset = this.get(tempArray1[i]);
                     this.registry.associate(asset.path, preAsset.path, "Predecessors");
-                    log.info("associations Predecessors");
-
                 }
-
             }
 
 
             //Adding Associatin for properties_specializations
             if (asset.attributes.properties_specializations != null) {
-
                 var tempArray2 = asset.attributes.properties_specializations.split("\,");
-
                 for (var i = 0; i < tempArray2.length; i++) {
-
                     var specAsset = this.get(tempArray2[i]);
                     this.registry.associate(asset.path, specAsset.path, "Specializations");
-                    log.info("associations Specializations");
-
                 }
-
             }
-
-
 
             //Adding Associatin for properties_generalizations
             if (asset.attributes.properties_generalizations != null) {
                 var tempArray3 = asset.attributes.properties_generalizations.split("\,");
-
                 for (var i = 0; i < tempArray3.length; i++) {
-
                     var genAsset = this.get(tempArray3[i]);
                     this.registry.associate(asset.path, genAsset.path, "Generalizations");
-                    log.info("associations Generalizations");
-
                 }
-
             }
 
 
             //Adding Associatin for properties_successor
             if (asset.attributes.properties_successors != null) {
                 var tempArray4 = asset.attributes.properties_successors.split("\,");
-
                 for (var i = 0; i < tempArray4.length; i++) {
-
                     var sucAsset = this.get(tempArray4[i]);
                     this.registry.associate(asset.path, sucAsset.path, "Successors");
-                    log.info("associations Successors");
-
                 }
             }
-
         }
     };
 };
@@ -148,10 +120,6 @@ asset.server = function(ctx) {
                 title: 'Lifecycle',
                 url: 'lifecycle',
                 path: 'lifecycle.jag'
-            }, {
-                title: 'Variant' + type,
-                url: 'variant',
-                path: 'variant.jag'
             }]
         }
     };
