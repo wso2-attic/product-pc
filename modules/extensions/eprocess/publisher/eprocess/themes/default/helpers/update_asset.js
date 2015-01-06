fd409f2ab8a8088d20db8ee7a36c10e202b16c51
Shiva Balachandran <shiva@wso2.com>
2015-01-06 15:02:42 +0530

    Revert "Adding the files for executable process"
    This reverts commit d1070a148f9a99050a5b73122b18137893e1b701.

--- modules/extensions/eprocess/publisher/eprocess/themes/default/helpers/update_asset.js
+++
@@ -1,7 +0,0 @@
-var resources = function (page, meta) {
-    return {
-       	js:['libs/jquery.form.min.js','update_asset.js','common/option_text.js','jquery.tokeninput.js','updateContentLoader.js','processContent.js','assetReplacer.js','tokenPopulator.js'], 
-       	css:['bootstrap-select.min.css','datepick/smoothness.datepick.css','grid.css']
-    };
-};
-
--- modules/extensions/eprocess/publisher/eprocess/themes/default/helpers/view-asset.js
+++
@@ -1,6 +0,0 @@
-var resources = function (block, page, area, meta) {
-    return {
-        js: ['jquery.MetaData.js', 'jquery.rating.pack.js', 'async.min.js', 'asset-core.js', 'asset.js', 'moment.min.js', 'porthole.min.js','jquery-1.8.3.min.js','assetReplacer.js'],
-        css: ['assets.css','grid.css','styles.css','font-awesome-ie7.min.css','jslider.css','jslider.round.plastic.css','navigation.css','sort-assets.css'],
-    };
-};
