a482262127f9677548bca1743183e3a7dd846786
Himasha Guruge <himashag@wso2.com>
2015-10-05 10:21:32 +0530

    PC store migration to ES 2.0

---
+++ modules/extensions/process/store/process/themes/store/js/listProcess.js
@@ -0,0 +1,55 @@
+/*
+ *  Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+/*
+ Functionality related to list created process content in Publisher
+ */
+var linkName; //store process name
+var href; // store link of the redirected page
+
+function getRelatedProcessContent(element) {
+    var checkName = $.trim(linkName);
+    $.ajax({
+        type: "GET",
+        url: "/publisher/assets/process/apis/content",
+        data: {
+            name: checkName,
+            reqType: "get"
+        }
+    });
+}
+// when the image is clicked get process name
+$('.ast-img').click(function (e) {
+    var element = $(this);
+    href = element.attr('href');
+    linkName = element[0].nextElementSibling.attributes[0].ownerElement.childNodes[1].innerText;
+    getgetRelatedProcessContent(element);
+    loadPageForLink(href);
+});
+// when link is clicked get  process name 
+$('.ast_name').click(function (e) {
+    var element = $(this);
+    linkName = element.text();
+    href = element.attr('href');
+    getRelatedProcessContent(element);
+    loadPageForLink(href);
+});
+
+function loadPageForLink(href) {
+    window.location = href;
+}
