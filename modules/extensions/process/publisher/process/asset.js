f810a77e32fd7121ad0e09144458437d072594c0
Himasha Guruge <himashag@wso2.com>
2015-10-08 11:41:26 +0530

    Fixing create/update redirect issue

--- modules/extensions/process/publisher/process/asset.js
+++ modules/extensions/process/publisher/process/asset.js
@@ -24,6 +24,11 @@ var assetLinks = function(user) {
 };
 
 asset.manager = function(ctx) {
+     var notifier = require('store').notificationManager;
+     var storeConstants = require('store').storeConstants;
+    var COMMENT = 'User comment';
+    var carbon = require('carbon');
+    var social = carbon.server.osgiService('org.wso2.carbon.social.core.service.SocialActivityService');
  return {
      create: function(options) {
          var ref = require('utils').time;
@@ -69,7 +74,68 @@ asset.manager = function(ctx) {
                  this.registry.associate(asset.path, sucAsset.path, "Successors");
              }
          }
-     }
+         var assetPath = asset.path;
+            var user = ctx.username;
+            var userRoles = ctx.userManager.getRoleListOfUser(user);
+            try {
+                social.warmUpRatingCache(ctx.assetType + ':' + options.id);
+            } catch (e) {
+                log.warn("Unable to publish the asset: " + ctx.assetType + ":" + options.id + " to social cache. This may affect on sort by popularity function.");
+            }
+            //Check whether the user has admin role
+            var endpoint = storeConstants.PRIVATE_ROLE_ENDPOINT + user;
+            for (var role in userRoles) {
+                if (userRoles.hasOwnProperty(role) && userRoles[role] == storeConstants.ADMIN_ROLE) {
+                    endpoint = storeConstants.ADMIN_ROLE_ENDPOINT;
+                }
+            }
+
+            var provider = ctx.username;
+
+            if(options.attributes.overview_provider){
+                provider = options.attributes.overview_provider;
+            }
+            provider = provider.replace(':', '@');
+            //Subscribe the asset author for LC update event and asset update event
+            notifier.subscribeToEvent(provider, assetPath, endpoint, storeConstants.LC_STATE_CHANGE);
+            notifier.subscribeToEvent(provider, assetPath, endpoint, storeConstants.ASSET_UPDATE);
+     },
+     update: function(options) {
+            this._super.update.call(this, options);
+            var asset = this.get(options.id); //TODO avoid get: expensive operation
+
+            if (asset.attributes.properties_predecessors != null) {
+             var listOfPredecessors = asset.attributes.properties_predecessors.split("\,");
+             for (var i = 0; i < listOfPredecessors.length; i++) {
+                 var preAsset = this.get(listOfPredecessors[i]);
+                 this.registry.associate(asset.path, preAsset.path, "Predecessors");
+             }
+         }
+         if (asset.attributes.properties_successors != null) {
+             var listOfSuccessors = asset.attributes.properties_successors.split("\,");
+             for (var i = 0; i < listOfSuccessors.length; i++) {
+                 var sucAsset = this.get(listOfSuccessors[i]);
+                 this.registry.associate(asset.path, sucAsset.path, "Successors");
+             }
+         }
+          //Adding associations for properties_generalizations
+         if (asset.attributes.properties_generalizations != null) {
+             var listOfGeneralizations = asset.attributes.properties_generalizations.split("\,");
+             for (var i = 0; i < listOfGeneralizations.length; i++) {
+                 var genAsset = this.get(listOfGeneralizations[i]);
+                 this.registry.associate(asset.path, genAsset.path, "Generalizations");
+             }
+         }
+         if (asset.attributes.properties_specializations != null) {
+             var listOfSpecializations = asset.attributes.properties_specializations.split("\,");
+             for (var i = 0; i < listOfSpecializations.length; i++) {
+                 var specAsset = this.get(listOfSpecializations[i]);
+                 this.registry.associate(asset.path, specAsset.path, "Specializations");
+             }
+         }
+            //trigger notification on asset update
+            notifier.notifyEvent(storeConstants.ASSET_UPDATE_EVENT, asset.type, asset.name, null, asset.path, ctx.tenantId);
+        }
  };
 };
 
--- modules/extensions/process/publisher/process/themes/default/helpers/update_asset.js
+++ modules/extensions/process/publisher/process/themes/default/helpers/update_asset.js
@@ -19,8 +19,8 @@
 
 var resources = function (page, meta) {
     return {
-       	js:['libs/jquery.form.min.js','update_asset.js','updateContentLoader.js','processContent.js','tokenPopulator.js','tinymce/tinymce.min.js'], 
-       	css:['bootstrap-select.min.css','datepick/smoothness.datepick.css','grid.css']
+       	js:['libs/jquery.form.min.js','publisher-utils.js','update_asset.js','date_picker/datepicker.js','select2.min.js','tags/tags-common.js','tags/tags-init-update-asset.js','notify.min.js','messages.js','updateContentLoader.js','processContent.js','tokenPopulator.js','tinymce/tinymce.min.js'], 
+       	css:['bootstrap-select.min.css','datepick/smoothness.datepick.css','date_picker/datepicker/base.css', 'date_picker/datepicker/clean.css','select2.min.css','grid.css']
     };
 };
 
