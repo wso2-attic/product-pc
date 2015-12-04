51a863acef96ab8fc3838f093745224f74292ed7
Isuru Wijesinghe <isuruwi@wso2.com>
2015-12-04 16:04:57 +0530

    Remove unnessary comments

--- modules/components/analytics/core/org.wso2.carbon.pc.analytics.core/src/main/java/org/wso2/carbon/pc/analytics/core/ProcessLevelMonitor.java
+++ modules/components/analytics/core/org.wso2.carbon.pc.analytics.core/src/main/java/org/wso2/carbon/pc/analytics/core/ProcessLevelMonitor.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2005-2015 WSO2, Inc. (http://wso2.com)
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,10 +33,6 @@ import java.util.Hashtable;
  */
 public class ProcessLevelMonitor {
 	private static final Log log = LogFactory.getLog(ProcessLevelMonitor.class);
-
-	{
-
-	}
 
 	/**
 	 * perform query: SELECT processDefinitionId, AVG(duration) AS avgExecutionTime FROM
