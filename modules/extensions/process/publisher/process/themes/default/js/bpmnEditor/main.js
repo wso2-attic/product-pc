6ea70179ecb78b30ece165fd130052846c68a96f
NatashaWso2 <natasha@wso2.com>
2016-03-15 10:56:30 +0530

    Adding license headers

--- modules/extensions/process/publisher/process/themes/default/js/bpmnEditor/main.js
+++ modules/extensions/process/publisher/process/themes/default/js/bpmnEditor/main.js
@@ -1,8 +1,26 @@
+/*
+ *  Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.w   See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
 var clickedShape;
 var updateStr = false;
 
 function update(str, id) {
-    // $('#' +id).css("backgroundColor", "white"); 
+
     clickedShape = str;
     if (clickedShape == 'circle') {
 
@@ -97,15 +115,6 @@ function setSeletedShape(id) {
   
     selectedShapeId = id;
 }
-
-
-/*
-window.onbeforeunload = function() {
-    return "Save the BPMN diagram  before leaving";
-};
-*/
-
-
 
 function setLinksOnUpload(links, activities) {
 
--- modules/extensions/process/publisher/process/themes/default/js/bpmnEditor/mainSVG.js
+++ modules/extensions/process/publisher/process/themes/default/js/bpmnEditor/mainSVG.js
@@ -1,18 +1,29 @@
-processEditor.prototype.refreshWindow = function(svg) {
+/*
+ *  Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.w   See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
 
-    var y = window.innerHeight;
-    svg.attr("width", "100%").attr("height", y);
-};
 var y = window.innerHeight;
 var xLoc = 500/2 - 25,
       yLoc = y/4;
-/*
-    var width = window.innerWidth ;*/
+
 var height = window.innerHeight;
 var activities = [{id: 0, title: "Activity",  x: xLoc, y: yLoc, activityType:"circle", radius:30}];
 var links = [];
-var arr = new Array();
-
 // Main svg 
 var mainDiv = d3.select("#svgM");
 var svg = mainDiv.append("svg")
