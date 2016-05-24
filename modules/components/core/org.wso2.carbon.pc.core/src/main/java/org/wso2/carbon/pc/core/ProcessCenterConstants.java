7b1139a56d9d4bc0cc813738201ecd63e429284d
Chathura Ekanayake <chathura.ekanayake@gmail.com>
2016-05-24 14:27:12 +0530

    Added reverse associations for inter-process associations. JIRA - https://wso2.org/jira/browse/PC-100

--- modules/components/core/org.wso2.carbon.pc.core/src/main/java/org/wso2/carbon/pc/core/ProcessCenterConstants.java
+++ modules/components/core/org.wso2.carbon.pc.core/src/main/java/org/wso2/carbon/pc/core/ProcessCenterConstants.java
@@ -3,7 +3,7 @@ package org.wso2.carbon.pc.core;
 /**
  * Holds the constants of ProcessStore
  */
-public class ProcessStoreConstants {
+public class ProcessCenterConstants {
     public static final String GREG_PATH = "/_system/governance/";
     public static final String PROCESS_ASSET_ROOT = "processes/";
     public static final String BPMN_CONTENT_PATH = "bpmncontent/";
