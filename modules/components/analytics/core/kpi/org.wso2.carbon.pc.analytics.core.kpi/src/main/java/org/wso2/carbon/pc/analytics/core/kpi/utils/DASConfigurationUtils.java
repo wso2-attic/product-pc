ff20039a90a5556233205d05f17c351dd94f13c1
warunalakshitha <warudanu@gmail.com>
2016-08-01 09:10:06 +0530

    Add process specific analytics enable methods

--- modules/components/analytics/core/kpi/org.wso2.carbon.pc.analytics.core.kpi/src/main/java/org/wso2/carbon/pc/analytics/core/kpi/utils/DASConfigurationUtils.java
+++ modules/components/analytics/core/kpi/org.wso2.carbon.pc.analytics.core.kpi/src/main/java/org/wso2/carbon/pc/analytics/core/kpi/utils/DASConfigurationUtils.java
@@ -16,10 +16,21 @@
 
 package org.wso2.carbon.pc.analytics.core.kpi.utils;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.wso2.carbon.pc.analytics.core.kpi.AnalyticsConfigConstants;
+import org.wso2.carbon.pc.analytics.core.kpi.internal.PCAnalyticsServerHolder;
+import org.wso2.carbon.pc.core.ProcessCenterConstants;
 import org.wso2.carbon.pc.core.ProcessCenterException;
+import org.wso2.carbon.pc.core.ProcessStore;
+import org.wso2.carbon.registry.core.exceptions.RegistryException;
+import org.wso2.carbon.registry.core.service.RegistryService;
+import org.wso2.carbon.registry.core.session.UserRegistry;
 
 import javax.xml.bind.DatatypeConverter;
+import javax.xml.transform.TransformerException;
 import java.nio.charset.StandardCharsets;
 
 import static org.wso2.carbon.pc.analytics.core.kpi.internal.PCAnalyticsServerHolder.getInstance;
@@ -28,6 +39,7 @@ import static org.wso2.carbon.pc.analytics.core.kpi.internal.PCAnalyticsServerHo
  * Utils file for Analytics configuration related properties
  */
 public class DASConfigurationUtils {
+    private static final Log log = LogFactory.getLog(DASConfigurationUtils.class);
 
     // Make the constructor private, since it is a utility class
     private DASConfigurationUtils() {
@@ -36,11 +48,100 @@ public class DASConfigurationUtils {
     /**
      * Check whether analytics configurations (with DAS) is made for the respective process
      *
+     * @param processName
+     * @param processVersion
      * @return isDASAnalyticsConfigured
+     * @throws ProcessCenterException
      */
-    public static boolean isDASAnalyticsConfigured()
+    public static boolean isDASAnalyticsConfigured(String processName, String processVersion)
             throws ProcessCenterException {
-        return getInstance().getProcessCenter().getProcessCenterConfiguration().isAnalyticsEnabled();
+        String processContent = null;
+        ProcessStore ps = new ProcessStore();
+        try {
+            RegistryService registryService = PCAnalyticsServerHolder.getInstance().getRegistryService();
+            if (registryService != null) {
+                UserRegistry reg = registryService.getGovernanceSystemRegistry();
+                String processAssetPath = ProcessCenterConstants.PROCESS_ASSET_ROOT + processName + "/" +
+                        processVersion;
+                org.wso2.carbon.registry.core.Resource resource = reg.get(processAssetPath);
+                processContent = new String((byte[]) resource.getContent());
+                Document doc = ps.stringToXML(processContent);
+
+                Element rootElement = doc.getDocumentElement();
+                Element propertiesElement = (Element) rootElement.getElementsByTagName("properties").item(0);
+
+                if (propertiesElement.getElementsByTagName(AnalyticsConfigConstants.IS_DAS_CONFIGED_TAG).getLength() > 0
+                        && propertiesElement.getElementsByTagName(AnalyticsConfigConstants.IS_DAS_CONFIGED_TAG).item(0)
+                        .getTextContent().equals("true")) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        } catch (RegistryException e) {
+            String errMsg = "Governance Registry access error, wile checking isDasConfigedForAnalytics";
+            throw new ProcessCenterException(errMsg, e);
+        } catch (Exception e) {
+            String errMsg = "Registry process.rxt String to XML conversion error, wile checking " +
+                    "isDasConfigedForAnalytics ";
+            throw new ProcessCenterException(errMsg, e);
+        }
+        return false;
+    }
+
+    /**
+     * Set the Property isDasConfigedForAnalytics in the process related .rxt to flag that analytics configurations
+     * (with DAS) is made for the respective process
+     *
+     * @param processName
+     * @param processVersion
+     * @throws ProcessCenterException
+     */
+    public static void setPropertyDASAnalyticsConfigured(String processName, String processVersion)
+            throws ProcessCenterException {
+        String processContent = null;
+        ProcessStore ps = new ProcessStore();
+        try {
+            RegistryService registryService = PCAnalyticsServerHolder.getInstance().getRegistryService();
+            if (registryService != null) {
+                UserRegistry reg = registryService.getGovernanceSystemRegistry();
+                String processAssetPath = ProcessCenterConstants.PROCESS_ASSET_ROOT + processName + "/" +
+                        processVersion;
+                org.wso2.carbon.registry.core.Resource resource = reg.get(processAssetPath);
+                processContent = new String((byte[]) resource.getContent());
+                Document doc = ps.stringToXML(processContent);
+
+                Element rootElement = doc.getDocumentElement();
+                Element propertiesElement = (Element) rootElement.getElementsByTagName("properties").item(0);
+
+                //add a new property item element if it is not existing already
+                if (propertiesElement.getElementsByTagName(AnalyticsConfigConstants.IS_DAS_CONFIGED_TAG).getLength()
+                        == 0) {
+                    ps.appendText(doc, propertiesElement, AnalyticsConfigConstants.IS_DAS_CONFIGED_TAG,
+                            ProcessCenterConstants.MNS, "true");
+
+                    String newProcessContent = ps.xmlToString(doc);
+                    resource.setContent(newProcessContent);
+                    reg.put(processAssetPath, resource);
+
+                    if (log.isDebugEnabled()) {
+                        log.debug("isDasConfigedForAnalytics property in process.rxt set as true");
+                    }
+                }
+            }
+        } catch (TransformerException | RegistryException e) {
+            String errMsg = "Exception in setting property isDASAnalyticsConfigured in process.rxt for the process:"
+                    + processName + ":" + processVersion;
+            log.error(errMsg, e);
+            throw new ProcessCenterException(errMsg, e);
+        } catch (Exception e) {
+            String errMsg =
+                    "Exception in setting property isDASAnalyticsConfigured in process.rxt while converting xml to " +
+                            "string for the process:"
+                            + processName + ":" + processVersion;
+            log.error(errMsg, e);
+            throw new ProcessCenterException(errMsg, e);
+        }
     }
 
     /**
